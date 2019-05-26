b8b03a6b9362b5e24d522b2608cbd5bcdb61f425
AlbertAtHome <494470187@qq.com>
2019-05-26 23:51:49 +0800

    add ex

---
+++ ex/ex1.py
@@ -0,0 +1,8 @@
+# this is a new beginning of my python career
+print("Hello World!",end='\n\n')
+print("Hello Again",end=' ')
+print("I like type this.")
+print("This is fun.")
+print("Yay! Printing")
+print("I'd much rater you 'not'.")
+print('I "said" do not touch this.')
