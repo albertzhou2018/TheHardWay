317bfb424b20c4eab57f7b0818173ef1337e36fe
AlbertAtHome <494470187@qq.com>
2019-05-28 23:03:24 +0800

    modified ex13, add input()

--- ex/ex13.py
+++ ex/ex13.py
@@ -6,3 +6,6 @@ print("The script is called:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
+
+age = input("How old are you?")
+print(f"I see, you are {age} years old")
